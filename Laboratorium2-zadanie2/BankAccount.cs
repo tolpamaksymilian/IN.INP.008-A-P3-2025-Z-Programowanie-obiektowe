dd65f30a1808c7e8eb58ce3560050f0c2d5c4792
tolpamaksymilian <maksymiliantolpa1tp@gmail.com>
2025-10-27 12:13:34 +0100

    Laboratorium 2 zadanie 2
    Dodano zdanie 2 z laboratorium 2

---
+++ Laboratorium2-zadanie2/BankAccount.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Zadanie 2.
+//Napisz klasę BankAccount, która będzie symulowała konto bankowe.
+//• Zaimplementuj właściwości Saldo (publiczne, tylko do odczytu) i Wlasciciel.
+//• Dodaj metodę Wplata(decimal kwota), która pozwala na zwiększenie salda, oraz metodę
+//Wyplata(decimal kwota), która sprawdzi, czy jest wystarczająca ilość środków, a następnie
+//odejmie odpowiednią kwotę.
+//• Użyj operatorów dostępu, aby zabezpieczyć saldo przed bezpośrednią modyfikacją.
+//Przykład użycia:
+//BankAccount konto = new BankAccount("Jan Kowalski", 1000);
+//konto.Wplata(500);
+//konto.Wyplata(200);
+//Console.WriteLine($"Saldo: {konto.Saldo}");
+
+namespace Laboratorium2_zadanie2
+{
+    internal class BankAccount
+    {
+
+        public decimal Saldo { get; private set; }
+        public string Wlasciciel { get; set; }
+        public BankAccount(string wlasciciel, decimal poczatkoweSaldo)
+        {
+            Wlasciciel = wlasciciel;
+            Saldo = poczatkoweSaldo;
+        }
+        public void Wplata(decimal kwota)
+        {
+            if (kwota > 0)
+            {
+                Saldo += kwota;
+                Console.WriteLine($"Wpłacono: {kwota}. Nowe saldo: {Saldo}");
+            }
+            else
+            {
+                throw new ArgumentException("Kwota wpłaty musi być większa od zera.");
+            }
+        }
+        public void Wyplata(decimal kwota)
+        {
+            if (kwota > 0)
+            {
+                if (kwota <= Saldo)
+                {
+                    Saldo -= kwota;
+                    Console.WriteLine($"Wypłacono: {kwota}. Nowe saldo: {Saldo}");
+                }
+                else
+                {
+                    throw new InvalidOperationException("Niewystarczające środki na koncie.");
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Kwota wypłaty musi być większa od zera.");
+            }
+        }
+        public void wyswietlSaldo()
+        {
+            Console.WriteLine($"Saldo: {Saldo}");
+        }
+
+
+
+    }
+}
---
+++ Laboratorium2-zadanie2/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Laboratorium2_zadanie2;
+
+BankAccount konto = new BankAccount("Jan Kowalski", 1000);
+konto.Wplata(500);
+konto.Wyplata(200);
+konto.wyswietlSaldo();
