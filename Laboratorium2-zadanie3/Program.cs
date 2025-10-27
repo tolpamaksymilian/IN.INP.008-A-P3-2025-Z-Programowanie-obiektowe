68d3f8e394430477234e7ed5b0d583bb38753370
tolpamaksymilian <maksymiliantolpa1tp@gmail.com>
2025-10-27 12:26:32 +0100

    Laboratorium 2 zadanie 3
    Dodano zadanie 3 z laboratorium 2

---
+++ Laboratorium2-zadanie3/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Laboratorium2_zadanie3;
+
+Student student = new Student("Jan", "Kowalski");
+student.DodajOcene(4);
+student.DodajOcene(5);
+student.DodajOcene(3);
+student.DodajOcene(3);
+student.DodajOcene(1);
+student.DodajOcene(2);
+Console.WriteLine($"Student: {student.Imie} {student.Nazwisko}\nŚrednia jego ocen wynosi: {student.SredniaOcen}");
---
+++ Laboratorium2-zadanie3/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 
+/// Autor: Maksymilian Tołpa
+/// Wersja: 1.0
+/// Zadanie: Laboratorium 2, Zadanie 3
+/// 
+/// Polecenie:
+/// Napisz klasę Student, która będzie przechowywała imię, nazwisko i tablicę ocen.
+///• Zaimplementuj właściwość SredniaOcen, która obliczy i zwróci średnią ocen.
+///• Dodaj metodę DodajOcene(int ocena), która doda nową ocenę do tablicy.
+///• Zaimplementuj konstruktor inicjujący imię i nazwisko studenta.
+/// </summary>
+
+namespace Laboratorium2_zadanie3
+{
+    internal class Student
+    {
+
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        private List<int> oceny;
+
+        public Student(string imie, string nazwisko)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            oceny = new List<int>();
+        }
+        public double SredniaOcen
+        {
+            get
+            {
+                if (oceny.Count == 0)
+                    return 0;
+                return oceny.Average();
+            }
+        }
+        public void DodajOcene(int ocena)
+        {
+            oceny.Add(ocena);
+        }
+
+    }
+}
