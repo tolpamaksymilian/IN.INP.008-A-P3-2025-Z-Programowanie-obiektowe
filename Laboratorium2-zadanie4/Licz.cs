be276f3c2d4e42524cc1bb5f5f4f5b83cd5f9f1d
tolpamaksymilian <maksymiliantolpa1tp@gmail.com>
2025-10-27 12:33:09 +0100

    Laboratorium 2 zadanie 4
    Dodano zadanie 4 z laboratorium 2

---
+++ Laboratorium2-zadanie4/Licz.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorium2_zadanie4
+{
+
+    /// <summary>
+    /// 
+    /// Autor: Maksymilian Tołpa
+    /// Wersja: 1.0
+    /// 
+    /// Zadanie: Zadanie 4 - Laboratorium 2
+    /// 
+    /// Polecenie:
+    /// Stwórz klasę Licz z:
+    /// • publicznym polem value przechowującym wartość liczbową.
+    /// • metodą Dodaj przyjmującą jeden parametr i dodającą przekazaną wartość do wartości
+    /// trzymanej w polu value.
+    /// • analogiczną operację odejmij
+    /// W Main utwórz kilka obiektów klasy Licz i wykonaj różne operacje.
+    /// Do klasy Licz dodaj konstruktor z jednym parametrem - który inicjuje pole wartość na liczbę przekazaną
+    /// w parametrze.
+    /// Zmień widoczność pola na private i dodaj funkcję wypisującą stan obiektu(pole value).
+    /// 
+    /// </summary>
+
+    internal class Licz
+    {
+
+        private int value;
+        public Licz(int initialValue)
+        {
+            value = initialValue;
+        }
+        public void Dodaj(int toAdd)
+        {
+            value += toAdd;
+        }
+        public void Odejmij(int toSubtract)
+        {
+            value -= toSubtract;
+        }
+        public void WypiszStan()
+        {
+            Console.WriteLine("Aktualna wartość: " + value);
+        }
+
+
+    }
+}
---
+++ Laboratorium2-zadanie4/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Laboratorium2_zadanie4;
+
+Licz liczba1 = new Licz(10);
+
+liczba1.Dodaj(5);
+liczba1.WypiszStan();
+liczba1.Odejmij(3);
+liczba1.WypiszStan();
