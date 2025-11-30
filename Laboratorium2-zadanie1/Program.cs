b4e0c6eb3b4cfd22a6b191ab2dde49637037a596
tolpamaksymilian <maksymiliantolpa1tp@gmail.com>
2025-11-30 10:45:12 +0100

    Update Program.cs

--- Laboratorium2-zadanie1/Program.cs
+++ Laboratorium2-zadanie1/Program.cs
@@ -5,5 +5,7 @@ using Laboratorium2_zadanie1;
 Osoba osoba1 = new Osoba("Jan", "Kowalski", 30);
 Osoba osoba2 = new Osoba("Anna", "Nowak", 25);
 
+//wykonanie
+
 osoba1.WyswietlInformacje();
 osoba2.WyswietlInformacje();
