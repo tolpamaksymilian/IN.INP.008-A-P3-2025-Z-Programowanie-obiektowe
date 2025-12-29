701bc5e281fd126baaf2b9ddfad72f3bf3ef1408
tolpamaksymilian <maksymiliantolpa1tp@gmail.com>
2025-12-29 14:36:19 +0100

    Dodano klasy Package oraz Payment

---
+++ TransportBooking/TransportBooking.Domain/Entities/Package.cs
@@ -0,0 +1,9 @@
+﻿namespace TransportBooking.Domain.Entities;
+
+public class Package
+{
+    public long PackageId { get; set; }
+    public long ReservationId { get; set; }
+    public decimal WeightKg { get; set; }
+    public string Description { get; set; } = "";
+}
---
+++ TransportBooking/TransportBooking.Domain/Entities/Payment.cs
@@ -0,0 +1,11 @@
+﻿namespace TransportBooking.Domain.Entities;
+
+public class Payment
+{
+    public long PaymentId { get; set; }
+    public long ReservationId { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime? PaidAt { get; set; }
+    public string Method { get; set; } = "";
+    public string PayStatus { get; set; } = "";
+}
