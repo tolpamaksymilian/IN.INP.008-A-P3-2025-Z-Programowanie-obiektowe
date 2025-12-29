130efc5c63365aeec08d8a082686f5892a1a87c0
tolpamaksymilian <maksymiliantolpa1tp@gmail.com>
2025-12-29 14:34:28 +0100

    Create Client.cs

---
+++ TransportBooking/TransportBooking.Domain/Entities/Client.cs
@@ -0,0 +1,14 @@
+﻿namespace TransportBooking.Domain.Entities;
+
+public class Client
+{
+    public long ClientId { get; set; }
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string Phone { get; set; } = "";
+    public string Address { get; set; } = "";
+    public string PostalCode { get; set; } = "";
+    public string City { get; set; } = "";
+    public DateTime CreatedAt { get; set; }
+}
