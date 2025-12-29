06ebdbf39fe2e53519c585c7c9f3eb147c7fdd8a
tolpamaksymilian <maksymiliantolpa1tp@gmail.com>
2025-12-29 11:52:45 +0100

    Rozpoczęcie projektu
    Utworzenie podstawowych plików i rozpoczęcie projektu

---
+++ Projekt/Projekt-Programowanie-Obiektowe/Projekt-Programowanie-Obiektowe/Form1.cs
@@ -0,0 +1,10 @@
+namespace Projekt_Programowanie_Obiektowe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
