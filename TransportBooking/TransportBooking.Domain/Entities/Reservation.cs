570fc58561621e24da27aad5083aa6234891e9e0
tolpamaksymilian <maksymiliantolpa1tp@gmail.com>
2025-12-29 14:35:35 +0100

    Create Reservation.cs

---
+++ TransportBooking/TransportBooking.Domain/Entities/Reservation.cs
@@ -0,0 +1,11 @@
+﻿namespace TransportBooking.Domain.Entities;
+
+public class Reservation
+{
+    public long ReservationId { get; set; }
+    public long ClientId { get; set; }
+    public long RouteId { get; set; }
+    public string ServiceType { get; set; } = "";
+    public string Status { get; set; } = "";
+    public DateTime CreatedAt { get; set; }
+}
