f5e31916dd9f163be2eb157b63a029d03ca09769
tolpamaksymilian <maksymiliantolpa1tp@gmail.com>
2025-12-29 13:06:25 +0100

    Create IClientService.cs

---
+++ Projekt/TransportBookingApp/TransportBooking.Services/Interfaces/IClientService.cs
@@ -0,0 +1,13 @@
+﻿using TransportBooking.Domain.Entities;
+
+namespace TransportBooking.Services.Interfaces;
+
+public interface IClientService
+{
+    List<Client> GetAll();
+    Client? GetById(int id);
+
+    Client Add(Client client);
+    void Update(Client client);
+    void Delete(int id);
+}
