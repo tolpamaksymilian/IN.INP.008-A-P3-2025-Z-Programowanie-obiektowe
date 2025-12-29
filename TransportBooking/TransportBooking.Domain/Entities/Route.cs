71acdee3dc6d4e2e76c072ee8f384a542e507147
tolpamaksymilian <maksymiliantolpa1tp@gmail.com>
2025-12-29 14:35:19 +0100

    Dodano Route oraz Vehicle ( klasy )

---
+++ TransportBooking/TransportBooking.Domain/Entities/Route.cs
@@ -0,0 +1,11 @@
+﻿namespace TransportBooking.Domain.Entities;
+
+public class Route
+{
+    public long RouteId { get; set; }
+    public long VehicleId { get; set; }
+    public string StartCity { get; set; } = "";
+    public string EndCity { get; set; } = "";
+    public DateTime DepartureTime { get; set; }
+    public decimal PricePerson { get; set; }
+}
---
+++ TransportBooking/TransportBooking.Domain/Entities/Vehicle.cs
@@ -0,0 +1,10 @@
+﻿namespace TransportBooking.Domain.Entities;
+
+public class Vehicle
+{
+    public long VehicleId { get; set; }
+    public string PlateNumber { get; set; } = "";
+    public string Model { get; set; } = "";
+    public int Seats { get; set; }
+    public bool Active { get; set; }
+}
