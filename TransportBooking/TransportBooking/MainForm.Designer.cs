9f36e3d2030cb4933b52126565ddfdd71880d998
tolpamaksymilian <maksymiliantolpa1tp@gmail.com>
2025-12-30 10:29:02 +0100

    Aktualizacja wyglądu oraz dodanie nowej funkcjonalności w zakładce klienci

--- TransportBooking/TransportBooking/MainForm.Designer.cs
+++ TransportBooking/TransportBooking/MainForm.Designer.cs
@@ -33,6 +33,24 @@
             btnLoadClients = new Button();
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
+            btnDeleteClient = new Button();
+            btnSearchClient = new Button();
+            txtSearchClient = new TextBox();
+            label10 = new Label();
+            txtPostalCode = new TextBox();
+            label11 = new Label();
+            txtCity = new TextBox();
+            label9 = new Label();
+            txtAddress = new TextBox();
+            btnAddClient = new Button();
+            label8 = new Label();
+            txtPhone = new TextBox();
+            label7 = new Label();
+            txtLastName = new TextBox();
+            label6 = new Label();
+            txtEmail = new TextBox();
+            label5 = new Label();
+            txtFirstName = new TextBox();
             tabPage2 = new TabPage();
             tabPage3 = new TabPage();
             tabPage4 = new TabPage();
@@ -41,14 +59,14 @@
             tabPage7 = new TabPage();
             tabPage8 = new TabPage();
             tabPage9 = new TabPage();
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
             db_connect_info = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
             ((System.ComponentModel.ISupportInitialize)dgvClients).BeginInit();
             tabControl1.SuspendLayout();
-            tabPage2.SuspendLayout();
+            tabPage1.SuspendLayout();
             tabPage9.SuspendLayout();
             SuspendLayout();
             // 
@@ -65,17 +83,17 @@
             // dgvClients
             // 
             dgvClients.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvClients.Location = new Point(52, 35);
+            dgvClients.Location = new Point(326, 16);
             dgvClients.Name = "dgvClients";
             dgvClients.RowHeadersWidth = 51;
-            dgvClients.Size = new Size(563, 69);
+            dgvClients.Size = new Size(752, 219);
             dgvClients.TabIndex = 1;
             // 
             // btnLoadClients
             // 
-            btnLoadClients.Location = new Point(133, 120);
+            btnLoadClients.Location = new Point(326, 241);
             btnLoadClients.Name = "btnLoadClients";
-            btnLoadClients.Size = new Size(207, 29);
+            btnLoadClients.Size = new Size(752, 48);
             btnLoadClients.TabIndex = 2;
             btnLoadClients.Text = "Wczytaj klientów";
             btnLoadClients.UseVisualStyleBackColor = true;
@@ -95,27 +113,194 @@
             tabControl1.Location = new Point(12, 12);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(776, 343);
+            tabControl1.Size = new Size(1114, 430);
             tabControl1.TabIndex = 3;
             // 
             // tabPage1
             // 
+            tabPage1.Controls.Add(btnDeleteClient);
+            tabPage1.Controls.Add(btnSearchClient);
+            tabPage1.Controls.Add(txtSearchClient);
+            tabPage1.Controls.Add(label10);
+            tabPage1.Controls.Add(txtPostalCode);
+            tabPage1.Controls.Add(label11);
+            tabPage1.Controls.Add(txtCity);
+            tabPage1.Controls.Add(label9);
+            tabPage1.Controls.Add(txtAddress);
+            tabPage1.Controls.Add(btnLoadClients);
+            tabPage1.Controls.Add(btnAddClient);
+            tabPage1.Controls.Add(dgvClients);
+            tabPage1.Controls.Add(label8);
+            tabPage1.Controls.Add(txtPhone);
+            tabPage1.Controls.Add(label7);
+            tabPage1.Controls.Add(txtLastName);
+            tabPage1.Controls.Add(label6);
+            tabPage1.Controls.Add(txtEmail);
+            tabPage1.Controls.Add(label5);
+            tabPage1.Controls.Add(txtFirstName);
             tabPage1.Location = new Point(4, 29);
             tabPage1.Name = "tabPage1";
             tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(768, 310);
+            tabPage1.Size = new Size(1106, 397);
             tabPage1.TabIndex = 0;
-            tabPage1.Text = "tabPage1";
+            tabPage1.Text = "Klienci";
             tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteClient
+            // 
+            btnDeleteClient.Location = new Point(835, 304);
+            btnDeleteClient.Name = "btnDeleteClient";
+            btnDeleteClient.Size = new Size(243, 27);
+            btnDeleteClient.TabIndex = 17;
+            btnDeleteClient.Text = "Usuń zaznaczonego klienta";
+            btnDeleteClient.UseVisualStyleBackColor = true;
+            btnDeleteClient.Click += btnDeleteClient_Click;
+            // 
+            // btnSearchClient
+            // 
+            btnSearchClient.Location = new Point(665, 304);
+            btnSearchClient.Name = "btnSearchClient";
+            btnSearchClient.Size = new Size(164, 27);
+            btnSearchClient.TabIndex = 16;
+            btnSearchClient.Text = "Wyszukaj klienta";
+            btnSearchClient.UseVisualStyleBackColor = true;
+            btnSearchClient.Click += btnSearchClient_Click;
+            // 
+            // txtSearchClient
+            // 
+            txtSearchClient.Location = new Point(326, 304);
+            txtSearchClient.Name = "txtSearchClient";
+            txtSearchClient.Size = new Size(333, 27);
+            txtSearchClient.TabIndex = 15;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(182, 269);
+            label10.Name = "label10";
+            label10.Size = new Size(104, 20);
+            label10.TabIndex = 14;
+            label10.Text = "Kod pocztowy";
+            // 
+            // txtPostalCode
+            // 
+            txtPostalCode.Location = new Point(182, 292);
+            txtPostalCode.Name = "txtPostalCode";
+            txtPostalCode.Size = new Size(125, 27);
+            txtPostalCode.TabIndex = 13;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(25, 269);
+            label11.Name = "label11";
+            label11.Size = new Size(54, 20);
+            label11.TabIndex = 12;
+            label11.Text = "Miasto";
+            // 
+            // txtCity
+            // 
+            txtCity.Location = new Point(25, 292);
+            txtCity.Name = "txtCity";
+            txtCity.Size = new Size(125, 27);
+            txtCity.TabIndex = 11;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(25, 183);
+            label9.Name = "label9";
+            label9.Size = new Size(47, 20);
+            label9.TabIndex = 10;
+            label9.Text = "Adres";
+            // 
+            // txtAddress
+            // 
+            txtAddress.Location = new Point(25, 206);
+            txtAddress.Name = "txtAddress";
+            txtAddress.Size = new Size(282, 27);
+            txtAddress.TabIndex = 9;
+            // 
+            // btnAddClient
+            // 
+            btnAddClient.Location = new Point(25, 343);
+            btnAddClient.Name = "btnAddClient";
+            btnAddClient.Size = new Size(282, 48);
+            btnAddClient.TabIndex = 8;
+            btnAddClient.Text = "Dodaj klienta";
+            btnAddClient.UseVisualStyleBackColor = true;
+            btnAddClient.Click += btnAddClient_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(182, 108);
+            label8.Name = "label8";
+            label8.Size = new Size(113, 20);
+            label8.TabIndex = 7;
+            label8.Text = "Numer telefonu";
+            // 
+            // txtPhone
+            // 
+            txtPhone.Location = new Point(182, 131);
+            txtPhone.Name = "txtPhone";
+            txtPhone.Size = new Size(125, 27);
+            txtPhone.TabIndex = 6;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(182, 16);
+            label7.Name = "label7";
+            label7.Size = new Size(72, 20);
+            label7.TabIndex = 5;
+            label7.Text = "Nazwisko";
+            // 
+            // txtLastName
+            // 
+            txtLastName.Location = new Point(182, 39);
+            txtLastName.Name = "txtLastName";
+            txtLastName.Size = new Size(125, 27);
+            txtLastName.TabIndex = 4;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(25, 108);
+            label6.Name = "label6";
+            label6.Size = new Size(88, 20);
+            label6.TabIndex = 3;
+            label6.Text = "Adres email";
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(25, 131);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(125, 27);
+            txtEmail.TabIndex = 2;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(25, 16);
+            label5.Name = "label5";
+            label5.Size = new Size(38, 20);
+            label5.TabIndex = 1;
+            label5.Text = "Imię";
+            // 
+            // txtFirstName
+            // 
+            txtFirstName.Location = new Point(25, 39);
+            txtFirstName.Name = "txtFirstName";
+            txtFirstName.Size = new Size(125, 27);
+            txtFirstName.TabIndex = 0;
             // 
             // tabPage2
             // 
-            tabPage2.Controls.Add(dgvClients);
-            tabPage2.Controls.Add(btnLoadClients);
             tabPage2.Location = new Point(4, 29);
             tabPage2.Name = "tabPage2";
             tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(768, 198);
+            tabPage2.Size = new Size(1106, 397);
             tabPage2.TabIndex = 1;
             tabPage2.Text = "tabPage2";
             tabPage2.UseVisualStyleBackColor = true;
@@ -125,7 +310,7 @@
             tabPage3.Location = new Point(4, 29);
             tabPage3.Name = "tabPage3";
             tabPage3.Padding = new Padding(3);
-            tabPage3.Size = new Size(768, 198);
+            tabPage3.Size = new Size(1106, 397);
             tabPage3.TabIndex = 2;
             tabPage3.Text = "tabPage3";
             tabPage3.UseVisualStyleBackColor = true;
@@ -135,7 +320,7 @@
             tabPage4.Location = new Point(4, 29);
             tabPage4.Name = "tabPage4";
             tabPage4.Padding = new Padding(3);
-            tabPage4.Size = new Size(768, 198);
+            tabPage4.Size = new Size(1106, 397);
             tabPage4.TabIndex = 3;
             tabPage4.Text = "tabPage4";
             tabPage4.UseVisualStyleBackColor = true;
@@ -145,7 +330,7 @@
             tabPage5.Location = new Point(4, 29);
             tabPage5.Name = "tabPage5";
             tabPage5.Padding = new Padding(3);
-            tabPage5.Size = new Size(768, 198);
+            tabPage5.Size = new Size(1106, 397);
             tabPage5.TabIndex = 4;
             tabPage5.Text = "tabPage5";
             tabPage5.UseVisualStyleBackColor = true;
@@ -155,7 +340,7 @@
             tabPage6.Location = new Point(4, 29);
             tabPage6.Name = "tabPage6";
             tabPage6.Padding = new Padding(3);
-            tabPage6.Size = new Size(768, 198);
+            tabPage6.Size = new Size(1106, 397);
             tabPage6.TabIndex = 5;
             tabPage6.Text = "tabPage6";
             tabPage6.UseVisualStyleBackColor = true;
@@ -165,7 +350,7 @@
             tabPage7.Location = new Point(4, 29);
             tabPage7.Name = "tabPage7";
             tabPage7.Padding = new Padding(3);
-            tabPage7.Size = new Size(768, 198);
+            tabPage7.Size = new Size(1106, 397);
             tabPage7.TabIndex = 6;
             tabPage7.Text = "tabPage7";
             tabPage7.UseVisualStyleBackColor = true;
@@ -175,7 +360,7 @@
             tabPage8.Location = new Point(4, 29);
             tabPage8.Name = "tabPage8";
             tabPage8.Padding = new Padding(3);
-            tabPage8.Size = new Size(768, 310);
+            tabPage8.Size = new Size(1106, 397);
             tabPage8.TabIndex = 7;
             tabPage8.Text = "tabPage8";
             tabPage8.UseVisualStyleBackColor = true;
@@ -191,46 +376,10 @@
             tabPage9.Location = new Point(4, 29);
             tabPage9.Name = "tabPage9";
             tabPage9.Padding = new Padding(3);
-            tabPage9.Size = new Size(768, 310);
+            tabPage9.Size = new Size(1106, 397);
             tabPage9.TabIndex = 8;
             tabPage9.Text = "Test połączenia z bazą";
             tabPage9.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(20, 19);
-            label1.Name = "label1";
-            label1.Size = new Size(231, 20);
-            label1.TabIndex = 1;
-            label1.Text = "Nazwa bazy danych: transport_db";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(20, 49);
-            label2.Name = "label2";
-            label2.Size = new Size(210, 20);
-            label2.TabIndex = 2;
-            label2.Text = "Nazwa uzytkownika: username";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(20, 79);
-            label3.Name = "label3";
-            label3.Size = new Size(126, 20);
-            label3.TabIndex = 3;
-            label3.Text = "Hasło: ************";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(20, 108);
-            label4.Name = "label4";
-            label4.Size = new Size(74, 20);
-            label4.TabIndex = 4;
-            label4.Text = "Port: 5432";
             // 
             // db_connect_info
             // 
@@ -241,17 +390,54 @@
             db_connect_info.TabIndex = 5;
             db_connect_info.Text = "Wynik połączenia:";
             // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(20, 108);
+            label4.Name = "label4";
+            label4.Size = new Size(74, 20);
+            label4.TabIndex = 4;
+            label4.Text = "Port: 5432";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(20, 79);
+            label3.Name = "label3";
+            label3.Size = new Size(126, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Hasło: ************";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(20, 49);
+            label2.Name = "label2";
+            label2.Size = new Size(210, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Nazwa uzytkownika: username";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(20, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(231, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Nazwa bazy danych: transport_db";
+            // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(792, 454);
+            ClientSize = new Size(1156, 454);
             Controls.Add(tabControl1);
             Name = "MainForm";
-            Text = "Form1";
+            Text = "Panel Admina - Zarządzanie transportem";
             ((System.ComponentModel.ISupportInitialize)dgvClients).EndInit();
             tabControl1.ResumeLayout(false);
-            tabPage2.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
             tabPage9.ResumeLayout(false);
             tabPage9.PerformLayout();
             ResumeLayout(false);
@@ -277,5 +463,23 @@
         private Label label3;
         private Label label2;
         private Label label1;
+        private Button btnAddClient;
+        private Label label8;
+        private TextBox txtPhone;
+        private Label label7;
+        private TextBox txtLastName;
+        private Label label6;
+        private TextBox txtEmail;
+        private Label label5;
+        private TextBox txtFirstName;
+        private Label label10;
+        private TextBox txtPostalCode;
+        private Label label11;
+        private TextBox txtCity;
+        private Label label9;
+        private TextBox txtAddress;
+        private Button btnDeleteClient;
+        private Button btnSearchClient;
+        private TextBox txtSearchClient;
     }
 }
--- TransportBooking/TransportBooking/MainForm.cs
+++ TransportBooking/TransportBooking/MainForm.cs
@@ -14,6 +14,12 @@ public partial class MainForm : Form
         InitializeComponent();
     }
 
+    /// <summary>
+    /// Zakładka "Test połączenia z bazą danych"
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    //Testowanie połącznia z bazą danych
     private void btnTestDb_Click(object sender, EventArgs e)
     {
         try
@@ -38,19 +44,43 @@ public partial class MainForm : Form
         }
     }
 
+    //Ładowanie klientów
+    private void LoadClients(string? filter = null)
+    {
+        using var db = new AppDbContext();
 
+        var q = db.Clients.AsNoTracking().AsQueryable();
+
+        if (!string.IsNullOrWhiteSpace(filter))
+        {
+            filter = filter.Trim().ToLower();
+
+            q = q.Where(c =>
+                (c.FirstName != null && c.FirstName.ToLower().Contains(filter)) ||
+                (c.LastName != null && c.LastName.ToLower().Contains(filter)) ||
+                (c.Email != null && c.Email.ToLower().Contains(filter)) ||
+                (c.Phone != null && c.Phone.ToLower().Contains(filter)) ||
+                (c.City != null && c.City.ToLower().Contains(filter))
+            );
+        }
+
+        dgvClients.DataSource = q
+            .OrderByDescending(c => c.ClientId)
+            .ToList();
+    }
+
+
+    /// <summary>
+    /// Zakładka "Klienci"
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    //Przycisk łądujący klientów z bazdy danych
     private void btnLoadClients_Click(object sender, EventArgs e)
     {
         try
         {
-            using var db = new AppDbContext();
-
-            var clients = db.Clients
-                .AsNoTracking()
-                .OrderByDescending(c => c.ClientId)
-                .ToList();
-
-            dgvClients.DataSource = clients;
+            LoadClients();
         }
         catch (Exception ex)
         {
@@ -58,6 +88,195 @@ public partial class MainForm : Form
         }
     }
 
+    //Przycisk dodający nowego klienta
+    private void btnAddClient_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            string firstName = txtFirstName.Text.Trim();
+            string lastName = txtLastName.Text.Trim();
 
+            string? email = string.IsNullOrWhiteSpace(txtEmail.Text) ? null : txtEmail.Text.Trim();
+            string? phone = string.IsNullOrWhiteSpace(txtPhone.Text) ? null : txtPhone.Text.Trim();
+
+            string? city = string.IsNullOrWhiteSpace(txtCity.Text) ? null : txtCity.Text.Trim();
+            string? address = string.IsNullOrWhiteSpace(txtAddress.Text) ? null : txtAddress.Text.Trim();
+            string? postalCode = string.IsNullOrWhiteSpace(txtPostalCode.Text) ? null : txtPostalCode.Text.Trim();
+
+            if (firstName.Length < 3)
+            {
+                MessageBox.Show("Imię musi mieć co najmniej 3 znaki.");
+                return;
+            }
+
+            if (lastName.Length < 3)
+            {
+                MessageBox.Show("Nazwisko musi mieć co najmniej 3 znaki.");
+                return;
+            }
+
+            if (email is not null)
+            {
+                int at = email.IndexOf('@');
+                int dot = email.LastIndexOf('.');
+                if (at <= 0 || dot <= at + 1 || dot == email.Length - 1)
+                {
+                    MessageBox.Show("Podaj poprawny adres e-mail.");
+                    return;
+                }
+            }
+
+            if (phone is not null)
+            {
+                string cleaned = phone.Replace(" ", "").Replace("-", "");
+                if (cleaned.StartsWith("+")) cleaned = cleaned[1..];
+
+                if (cleaned.Length != 9 || !cleaned.All(char.IsDigit))
+                {
+                    MessageBox.Show("Podaj poprawny numer telefonu (9 cyfr).");
+                    return;
+                }
+            }
+
+            if (postalCode is not null)
+            {
+                string pc = postalCode.Replace(" ", "");
+                bool ok =
+                    (pc.Length == 6 && pc[2] == '-' && pc.Take(2).All(char.IsDigit) && pc.Skip(3).All(char.IsDigit)) ||
+                    (pc.Length == 5 && pc.All(char.IsDigit));
+
+                if (!ok)
+                {
+                    MessageBox.Show("Kod pocztowy jest niepoprawny. Użyj formatu 00-000 lub 00000.");
+                    return;
+                }
+            }
+
+            if (email is not null && email.Length > 120) { MessageBox.Show("Email max 120 znaków."); return; }
+            if (phone is not null && phone.Length > 20) { MessageBox.Show("Telefon max 20 znaków."); return; }
+            if (address is not null && address.Length > 120) { MessageBox.Show("Adres max 120 znaków."); return; }
+            if (city is not null && city.Length > 60) { MessageBox.Show("Miasto max 60 znaków."); return; }
+            if (postalCode is not null && postalCode.Length > 10) { MessageBox.Show("Kod pocztowy max 10 znaków."); return; }
+
+            using var db = new AppDbContext();
+
+            if (email is not null)
+            {
+                bool exists = db.Clients.Any(c => c.Email == email);
+                if (exists)
+                {
+                    MessageBox.Show("Klient z takim adresem e-mail już istnieje.");
+                    return;
+                }
+            }
+
+            if (phone is not null)
+            {
+                bool exists = db.Clients.Any(c => c.Phone == phone);
+                if (exists)
+                {
+                    MessageBox.Show("Klient z takim numerem telefonu już istnieje.");
+                    return;
+                }
+            }
+
+            var client = new Client
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Email = email,
+                Phone = phone,
+                City = city,
+                Address = address,
+                PostalCode = postalCode,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            db.Clients.Add(client);
+            db.SaveChanges();
+
+            MessageBox.Show($"Dodano klienta ✅ ID: {client.ClientId}");
+
+            // czyść pola
+            txtFirstName.Clear();
+            txtLastName.Clear();
+            txtEmail.Clear();
+            txtPhone.Clear();
+            txtCity.Clear();
+            txtAddress.Clear();
+            txtPostalCode.Clear();
+
+            // odśwież listę
+            btnLoadClients_Click(sender, e);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Błąd: " + ex.Message);
+        }
+    }
+
+    //Przycisk wyszukiwujący klienta w bazie danych
+    private void btnSearchClient_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            LoadClients(txtSearchClient.Text);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Błąd: " + ex.Message);
+        }
+    }
+
+    //Przycisk usuwający klienta zaznaczonego
+    private void btnDeleteClient_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            if (dgvClients.CurrentRow == null)
+            {
+                MessageBox.Show("Zaznacz klienta w tabeli.");
+                return;
+            }
+
+            // Pobieramy ID z zaznaczonego wiersza (kolumna ClientId)
+            var idObj = dgvClients.CurrentRow.Cells["ClientId"].Value;
+            if (idObj == null || !long.TryParse(idObj.ToString(), out long clientId))
+            {
+                MessageBox.Show("Nie udało się odczytać ID klienta.");
+                return;
+            }
+
+            var confirm = MessageBox.Show(
+                $"Czy na pewno usunąć klienta ID: {clientId}?",
+                "Potwierdzenie usunięcia",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning);
+
+            if (confirm != DialogResult.Yes)
+                return;
+
+            using var db = new AppDbContext();
+
+            // Uwaga: jeśli klient ma rezerwacje -> FK może zablokować usunięcie (i dobrze)
+            var client = db.Clients.FirstOrDefault(c => c.ClientId == clientId);
+            if (client == null)
+            {
+                MessageBox.Show("Klient nie istnieje (może już został usunięty).");
+                LoadClients(txtSearchClient.Text);
+                return;
+            }
+
+            db.Clients.Remove(client);
+            db.SaveChanges();
+
+            MessageBox.Show("Usunięto klienta ✅");
+            LoadClients(txtSearchClient.Text);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Błąd: " + ex.Message);
+        }
+    }
 
 }
